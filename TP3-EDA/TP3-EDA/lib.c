b6c8ceaf9b3a249a7a884958cff1b01f6d4a0725
Manuel Mollon <mmollon@itba.edu.ar>
2017-08-30 17:52:25 -0300

    arreglo errores de compilacion

--- TP3-EDA/TP3-EDA/lib.c
+++ TP3-EDA/TP3-EDA/lib.c
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-#include <stdio.h>
-#include "lib.h"
-
-#define OPTION '-'
-#define EOW '\0'
-#define SPACE ' '
-
-int parseCmdLine(int argc, char *argv[], pCallback p, void *userData){
-
-	int i = 1, ret; 
-	int opt_and_params = 0;
-
-	while(i < argc){
-		if(*argv[i] == OPTION){ // is option
-			if((i+1 != argc)&&( *(argv[i]+1) != EOW)&&(*(argv[i]+1) != SPACE)){
-				
-				ret = p(argv[i]+1, argv[i+1], userData);
-				i += 2;
-			}
-			else{
-				ret = -1;
-			}
-		}
-		else{ // is parameter
-			
-			ret = p(NULL, argv[i], userData);
-			i++;
-		}
-
-		if((ret == -1)||(ret == 0)) // any error
-			return ret;
-		else
-			opt_and_params++; // count options and parameters
-	}
-
-	return opt_and_params;
-} 
-
-=======
 #include <stdio.h>
 #include "lib.h"
 
@@ -82,4 +36,3 @@ int parseCmdLine(int argc, char *argv[], pCallback p, void *userData){
 
 	return opt_and_params;
 }
->>>>>>> 4bdbf3251685d2946f8d85d509dc55566905a06d
--- TP3-EDA/TP3-EDA/lib.h
+++ TP3-EDA/TP3-EDA/lib.h
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   lib.h
- * Author: matias
- *
- * Created on August 29, 2017, 11:00 PM
- */
 
 #ifndef LIB_H
 #define LIB_H
@@ -24,28 +11,3 @@ int parseCmdLine(int argc, char *argv[], pCallback p, void *userData);
 
 #endif /* LIB_H */
 
-=======
-///////////////////////////////////////////
-//// --------------------------------- ////
-//// | LIBRERIA: Command line parser | ////
-//// --------------------------------- ////
-//// EDA - TP1 - Grupo 3               ////
-////  - NOWIK, Ariel                   ////
-////  - REGUEIRA, Marcelo              ////
-////  - VIJANDE, Ezequiel              ////
-///////////////////////////////////////////
-#ifndef LIB_H
-#define LIB_H
-
-typedef int (*pCallback) (char*,char*,void*);
-
-typedef struct{ /// parametro u opcion
-    int type;
-    char *key;
-    char *value;
-}t_action;
-
-int parseCmdLine(int argc, char *argv[], pCallback p, void *userData);
-
-#endif /* LIB_H */
->>>>>>> 4bdbf3251685d2946f8d85d509dc55566905a06d
